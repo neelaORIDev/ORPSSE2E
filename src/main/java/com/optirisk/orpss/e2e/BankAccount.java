8d83f1c164ef05cad0bd67ee1f67ec29083e111a
neelaORIDev <sneelakandan@optiriskindia.com>
2014-09-10 21:34:22 +0530

    Added Test Functionality

--- src/main/java/com/optirisk/orpss/e2e/BankAccount.java
+++ src/main/java/com/optirisk/orpss/e2e/BankAccount.java
@@ -2,13 +2,23 @@ package com.optirisk.orpss.e2e;
 
 public class BankAccount {
 
+	private String actHolderName = null;
+	private long actNumber = 0L;
 	private double actBalance = 0;
 	
-	public BankAccount(double myBalance){
-		this.actBalance = myBalance;
+	public BankAccount(String actHolderName, long actNumber){
+		this.actBalance = 0;
+		this.actHolderName = actHolderName;
+		this.actNumber = actNumber;
 	}
 	
-	public void debitBalance(double inAmount){
+	public void creditAmount(double inAmount){
+		this.actBalance += inAmount;
+		System.out.println("Amount "+inAmount+" has been added to your main balance;"
+				+ " Current Account Balance is "+this.actBalance);
+	}
+	
+	public void debitAmount(double inAmount){
 		
 		if(inAmount > this.actBalance){
 			
@@ -21,4 +31,55 @@ public class BankAccount {
 		System.out.println("Your Current Balance After Debited Amount "+inAmount+" : "+this.actBalance);
 		
 	}
+
+	/**
+	 * @return the actHolderName
+	 */
+	public String getActHolderName() {
+		return actHolderName;
+	}
+
+	/**
+	 * @param actHolderName the actHolderName to set
+	 */
+	public void setActHolderName(String actHolderName) {
+		this.actHolderName = actHolderName;
+	}
+
+	/**
+	 * @return the actNumber
+	 */
+	public long getActNumber() {
+		return actNumber;
+	}
+
+	/**
+	 * @param actNumber the actNumber to set
+	 */
+	public void setActNumber(long actNumber) {
+		this.actNumber = actNumber;
+	}
+
+	/**
+	 * @return the actBalance
+	 */
+	public double getActBalance() {
+		return actBalance;
+	}
+
+	/**
+	 * @param actBalance the actBalance to set
+	 */
+	public void setActBalance(double actBalance) {
+		this.actBalance = actBalance;
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString() {
+		return "BankAccount [actHolderName=" + actHolderName + ", actNumber="
+				+ actNumber + ", actBalance=" + actBalance + "]";
+	}	
 }
--- src/test/java/com/optirisk/orpss/test/BankAccountTest.java
+++ src/test/java/com/optirisk/orpss/test/BankAccountTest.java
@@ -1,6 +1,20 @@
 package com.optirisk.orpss.test;
 
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import com.optirisk.orpss.e2e.BankAccount;
+
 public class BankAccountTest {
 
-	
+   @Test
+   public void testAdd() {
+	   
+	  BankAccount myAccount = new BankAccount("Neelakandan",1234567890);
+	  
+	  myAccount.creditAmount(1000);
+	  
+      assertEquals(1000,myAccount.getActBalance());
+   }
 }
